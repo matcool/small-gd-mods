5356af7062ac0789a59d17bfcc75d8493100427e
matcool <26722564+matcool@users.noreply.github.com>
2021-04-25 18:14:20 -0300

    add attempt sum mod

---
+++ src/sum-attempts.cpp
@@ -0,0 +1,159 @@
+#define WIN32_LEAN_AND_MEAN
+#include "mapped-hooks.hpp"
+#include "Windows.h"
+#include <fstream>
+#include <iostream>
+#include <cocos2d.h>
+#include <gd.h>
+#include "utils.hpp"
+
+using namespace cocos2d;
+
+uintptr_t base;
+
+class GJGameLevel : public CCNode {
+public:
+    PAD(300);
+    int attempts;
+
+    PAD(104);
+    int levelFolder;
+};
+
+
+class GameLevelManager : public CCNode {
+public:
+    PAD(8);
+    CCDictionary* dict_saved_levels;
+
+    static inline auto sharedState() {
+        return *cast<GameLevelManager**>(base + 0x3222C8);
+    }
+};
+
+class LocalLevelManager : public CCNode {
+public:
+    PAD(36);
+    CCArray* levels_array;
+
+    static inline auto sharedState() {
+        return *cast<LocalLevelManager**>(base + 0x3222E4);
+    }
+};
+
+class GJSearchObject : public CCNode {
+public:
+    int search_type;
+    PAD(96);
+    int current_folder;
+};
+
+class LevelBrowserLayer : public CCLayer {
+public:
+	PAD(52);
+	GJSearchObject* search_object;
+};
+
+inline int get_attempt_count_for_folder(int folder, CCDictionary* dict) {
+    int total = 0;
+    CCDictElement* value;
+    CCDICT_FOREACH(dict, value) {
+        auto lvl = cast<GJGameLevel*>(value->getObject());
+        if (lvl && (!folder || lvl->levelFolder == folder)) {
+            total += lvl->attempts;
+        }
+    }
+    return total;
+}
+
+inline int get_attempt_count_for_folder(int folder, CCArray* arr) {
+    int total = 0;
+    CCObject* value;
+    CCARRAY_FOREACH(arr, value) {
+        auto lvl = cast<GJGameLevel*>(value);
+        if (lvl && (!folder || lvl->levelFolder == folder)) {
+            total += lvl->attempts;
+        }
+    }
+    return total;
+}
+
+class DaButtonCallback {
+public:
+    void callback(CCObject* sender) {
+        auto lvl_browser = cast<LevelBrowserLayer*>(this);
+        auto folder = lvl_browser->search_object->current_folder;
+        int total;
+        if (lvl_browser->search_object->search_type == 98) {
+            total = get_attempt_count_for_folder(folder, LocalLevelManager::sharedState()->levels_array);
+        } else {
+            total = get_attempt_count_for_folder(folder, GameLevelManager::sharedState()->dict_saved_levels);
+        }
+        // https://stackoverflow.com/a/24192835/9124836
+        auto str = std::to_string(total);
+        int n = str.length() - 3;
+        while (n > 0) {
+            str.insert(n, ",");
+            n -= 3;
+        }
+        str += " attempts";
+        gd::FLAlertLayer::create(nullptr, "Attempt Count", "OK", nullptr, str.c_str())->show();
+    }
+};
+
+bool __fastcall LevelBrowserLayer_init(CCLayer* self, void*, GJSearchObject* so) {
+    if (!MHook::getOriginal(LevelBrowserLayer_init)(self, 0, so)) return false;
+    if (so->search_type == 98 || so->search_type == 99) {
+        auto menu = CCMenu::create();
+        auto sprite = CCSprite::create("GJ_button_01.png");
+        auto skull = CCSprite::createWithSpriteFrameName("miniSkull_001.png");
+        skull->setPosition({20, 20});
+        sprite->addChild(skull);
+        auto btn = gd::CCMenuItemSpriteExtra::create(
+            sprite,
+            self,
+            menu_selector(DaButtonCallback::callback)
+        );
+        menu->addChild(btn);
+        menu->setPosition({30, 80});
+        self->addChild(menu);
+    }
+    return true;
+}
+
+// #define _DEBUG
+
+DWORD WINAPI my_thread(void* hModule) {
+#ifdef _DEBUG
+    AllocConsole();
+    std::ofstream conout("CONOUT$", std::ios::out);
+    std::ifstream conin("CONIN$", std::ios::in);
+    std::cout.rdbuf(conout.rdbuf());
+    std::cin.rdbuf(conin.rdbuf());
+#endif
+
+    MH_Initialize();
+
+    base = reinterpret_cast<uintptr_t>(GetModuleHandle(0));
+    
+    MHook::registerHook(base + 0x15a040, LevelBrowserLayer_init);
+
+    MH_EnableHook(MH_ALL_HOOKS);
+
+#ifdef _DEBUG
+    std::getline(std::cin, std::string());
+
+    MH_Uninitialize();
+    conout.close();
+    conin.close();
+    FreeConsole();
+    FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(hModule), 0);
+#endif
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+        CreateThread(0, 0x1000, my_thread, hModule, 0, 0);
+    return TRUE;
+}
