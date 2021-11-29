b6156ccd983c18d266e18db49eadd6e45bf824ff
matcool <26722564+matcool@users.noreply.github.com>
2021-11-29 13:03:52 -0300

    oops 2

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,8 +1,18 @@
 #pragma once
-#include <cstdint>
 
 template <typename R, typename T>
 inline R cast(T value) { return reinterpret_cast<R>(value); }
 
-inline auto follow(std::uintptr_t addr) { return *cast<std::uintptr_t*>(addr); }
+inline auto follow(uintptr_t addr) { return *cast<uintptr_t*>(addr); }
 inline auto follow(void* addr) { return *cast<void**>(addr); }
+
+// only use this when necessary
+template <typename T, typename U>
+T union_cast(U value) {
+    union {
+        T a;
+        U b;
+    } u;
+    u.b = value;
+    return u.a;
+}
